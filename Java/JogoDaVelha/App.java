2ef80f7786e62e219af53ef9449b9261cf286323
guilherme-fermino-de-souza <142915619+guilherme-fermino-de-souza@users.noreply.github.com>
2025-01-26 13:49:44 -0300

    Update App.java

--- Java/JogoDaVelha/App.java
+++ Java/JogoDaVelha/App.java
@@ -4,67 +4,76 @@ public class App {
 	public static void main(String[] args) {
 		Scanner ler = new Scanner(System.in);
 		// TODO Auto-generated method stub
-		int placarX = 0, placarO = 0, numRodadas = 0;
-		char jogadaAtual = 'o';
-			
-			for (int jogo = 0; jogo != 1;) { //Jogo
-				char[][] tabuleiro = {{' ', ' ', ' '},  //linha/coluna
-						{' ',' ',' '}, 
-						{' ', ' ', ' '}}; 
-				numRodadas++;
-				System.out.println("JOGO DA VELHA\n" + numRodadas + "° Rodada!");
-				for (int numeroJogadas = 0; numeroJogadas <= 9; numeroJogadas++) { //Partida
-					if (jogadaAtual=='o') {
-						jogadaAtual = 'x';
-					} else {
-						jogadaAtual = 'o';
-					}
-					System.out.println("\nJogador " + jogadaAtual + " Escolha uma Linha(0 a 2)");
+		int placarX = 0, placarO = 0,  rodada = 0;
+		char jogador = 'X';
+		char [][] tabuleiro = {{' ', ' ', ' '},
+								{' ',' ',' '},
+								{' ', ' ',' '}};
+		for (int i = 0 ; i<1;) {
+			System.out.println("Placar\n X " + placarX + " points       O " + placarO + " points");
+			rodada++;
+			System.out.println(rodada + "° Rodada!");
+			for (int jogo = 1; jogo < 10; jogo++) { //Jogo
+				for (boolean jogadaValida = false; jogadaValida != true;) { //Jogada Atual
+					System.out.println("Jogador " + jogador + " escolha uma Linha(0 a 2)");
 					int linha = ler.nextInt();
-					System.out.println("Jogador " + jogadaAtual + " Escolha uma Coluna(0 a 2)");
+					System.out.println("Jogador " + jogador + " escolha uma Coluna(0 a 2)");
 					int coluna = ler.nextInt();
-					
-					if (tabuleiro[linha][coluna] == ' ') { //Jogada Válida
-						tabuleiro[linha][coluna] = jogadaAtual;
-					} else {
-						System.out.println("JOGADA INVÁLIDA\n");
-					}
-					
-					if ( (tabuleiro[0][0] == jogadaAtual && tabuleiro[0][1] == jogadaAtual && tabuleiro[0][2] == jogadaAtual) //Verifica vencedor
-					|| (tabuleiro[1][0] == jogadaAtual && tabuleiro[1][1] == jogadaAtual && tabuleiro[1][2] == jogadaAtual)
-					|| (tabuleiro[2][0] == jogadaAtual && tabuleiro[2][1] == jogadaAtual && tabuleiro[2][2] == jogadaAtual)
-					|| (tabuleiro[0][0] == jogadaAtual && tabuleiro[1][0] == jogadaAtual && tabuleiro[2][0] == jogadaAtual)
-					|| (tabuleiro[0][1] == jogadaAtual && tabuleiro[1][1] == jogadaAtual && tabuleiro[2][1] == jogadaAtual)
-					|| (tabuleiro[0][2] == jogadaAtual && tabuleiro[1][2] == jogadaAtual && tabuleiro[2][2] == jogadaAtual)
-					|| (tabuleiro[0][0] == jogadaAtual && tabuleiro[1][1] == jogadaAtual && tabuleiro[2][2] == jogadaAtual)
-					|| (tabuleiro[0][2] == jogadaAtual && tabuleiro[1][1] == jogadaAtual && tabuleiro[2][0] == jogadaAtual)) {
-						switch (jogadaAtual) {
-							case 'x': placarX = placarX + 1;
-								break;
-							case 'o': placarO = placarO + 1;
-								break;			
+					if (3 > linha >> -1 && 3 > coluna >> -1) {
+						if (tabuleiro[linha][coluna] == ' ') {
+							tabuleiro[linha][coluna] = jogador;
+							for (int l = 0; l < 3; l++) {
+								for (int c = 0; c < 3; c++) {
+									System.out.print("|" + tabuleiro[l][c]);
+								}
+								System.out.print("|\n");
 							}
-						System.out.println("O JOGADOR " + jogadaAtual + " É O VENCEDOR!");
-						System.out.println("\n     * PLACAR *     \n" + "Jogador X " + placarX + "   |    Jogador O " + placarO + "\n");
-						break;
-					} else {
-						
-					}
-					
-					for (int i = 0; i < 3; i++) { //Tabuleiro
-						for (int j = 0; j < 3; j++) {
-							System.out.print("|" + tabuleiro[i][j]);
+							jogadaValida = true;
+	
+						} else {
+							System.out.println("\nEspaço Já Preenchido\n");
 						}
-						System.out.print("| \n");
+					} else {
+						System.out.println("\nNúmero Inválido\n");
 					}
 				}
-				System.out.println("Deseja sair do jogo(S/N)?");
-				String repetir = ler.next();
-				if (repetir.equalsIgnoreCase("S")) {
-					jogo = 1;
-					System.out.println("Saindo...");
+				
+				if ( (tabuleiro[0][0] == jogador && tabuleiro[0][1] == jogador && tabuleiro[0][2] == jogador) //Verifica vencedor
+					|| (tabuleiro[1][0] == jogador && tabuleiro[1][1] == jogador && tabuleiro[1][2] == jogador)
+					|| (tabuleiro[2][0] == jogador && tabuleiro[2][1] == jogador && tabuleiro[2][2] == jogador)
+					|| (tabuleiro[0][0] == jogador && tabuleiro[1][0] == jogador && tabuleiro[2][0] == jogador)
+					|| (tabuleiro[0][1] == jogador && tabuleiro[1][1] == jogador && tabuleiro[2][1] == jogador)
+					|| (tabuleiro[0][2] == jogador && tabuleiro[1][2] == jogador && tabuleiro[2][2] == jogador)
+					|| (tabuleiro[0][0] == jogador && tabuleiro[1][1] == jogador && tabuleiro[2][2] == jogador)
+					|| (tabuleiro[0][2] == jogador && tabuleiro[1][1] == jogador && tabuleiro[2][0] == jogador)) {
+					System.out.println("O Jogador " + jogador + " Venceu!!!");
+					switch (jogador) {
+					case 'X':
+						jogador = 'O';
+						placarX++;
+						break;
+					case 'O':
+						jogador = 'X';
+						placarO++;
+						break;
+					}
+					break;
+				} else {
+					switch (jogador) {
+					case 'X':
+						jogador = 'O';
+						break;
+					case 'O':
+						jogador = 'X';
+						break;
+					}
+				}
+				if (jogo == 9) {
+					System.out.print("Empate");
+					break;
 				}
 			}
+		}
 	}
 
 }
